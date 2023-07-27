549591541c46079f8f8ba2b48c9893b66d4cfdca
Mark Jeschke <markjeschke@yahoo.com>
2023-07-27 12:54:33 -0700

    Initial Commit

---
+++ SignUpView/SignUpViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignUpViewApp.swift
+//  SignUpView
+//
+//  Created by Mark Jeschke on 7/27/23.
+//
+
+import SwiftUI
+
+@main
+struct SignUpViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
